519ba0a0df8c46ccfccfe6e7ee95f1038e533812
Matthbo <matthijs.booman@hotmail.com>
2021-02-15 16:00:02 +0100

    Updated 15-02-2021

---
+++ SocketClient/Concurrent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using Sequential;
+using System.Collections.Generic;
+
+namespace Concurrent
+{
+    public class ConcurrentClient : SimpleClient
+    {
+        public Thread workerThread;
+
+        public ConcurrentClient(int id, Setting settings) : base(id, settings)
+        {
+            // todo [Assignment]: implement required code
+
+        }
+        public void run()
+        {
+            this.prepareClient();
+            this.communicate();
+        }
+    }
+    public class ConcurrentClientsSimulator : SequentialClientsSimulator
+    {
+        private ConcurrentClient[] clients;
+
+        public ConcurrentClientsSimulator() : base()
+        {
+            Console.Out.WriteLine("\n[ClientSimulator] Concurrent simulator is going to start with {0}", settings.experimentNumberOfClients);
+            clients = new ConcurrentClient[settings.experimentNumberOfClients];
+        }
+
+        public void ConcurrentSimulation()
+        {
+            List<Thread> threads = new List<Thread>();
+
+            // todo [Assignment]: implement required code
+            for (int i = 0; i < settings.experimentNumberOfClients; i++)
+            {
+                clients[i] = new ConcurrentClient(i + 1, settings); // id>0 means this is not a terminating client
+            }
+
+            foreach (ConcurrentClient client in clients)
+            {
+                threads.Add(new Thread(() => client.run()));
+            }
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.Out.WriteLine("\n[ClientSimulator] All clients finished with their communications ... ");
+
+            Thread.Sleep(settings.delayForTermination);
+
+            ConcurrentClient endClient = new ConcurrentClient(-1, settings); // this is a terminating client: it will terminate the whole simulation
+            endClient.prepareClient();
+            // todo 13: check what happens in server side after this client.
+            endClient.communicate();
+        }
+    }
+}
---
+++ SocketServer/Concurrent.cs
@@ -0,0 +1,142 @@
+﻿/* 
+    Class: INF3D
+    Matthijs Booman - 0964703
+    Bram Gussekloo - 0966476
+*/
+
+using Sequential;
+using System;
+//todo [Assignment]: add required namespaces
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Concurrent
+{
+    public class ConcurrentServer : SequentialServer
+    {
+        // todo [Assignment]: implement required attributes specific for concurrent server
+        private List<Thread> threads;
+        private List<string> votes;
+        private readonly object clientCountLock;
+        private readonly object threadCountLock;
+        private readonly object votesLock;
+
+        public ConcurrentServer(Setting settings) : base(settings)
+        {
+            // todo [Assignment]: implement required code
+            threads = new List<Thread>();
+            votes = new List<string>();
+            clientCountLock = new object();
+            threadCountLock = new object();
+            votesLock = new object();
+        }
+        public override void prepareServer()
+        {
+            // todo [Assignment]: implement required code
+            Console.WriteLine("[Server] is ready to start ...");
+    	    try{
+                localEndPoint = new IPEndPoint(this.ipAddress, settings.serverPortNumber);
+                listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(localEndPoint);
+                listener.Listen(settings.serverListeningQueue);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for incoming connections ... ");
+
+                    Socket connection = listener.Accept();
+
+                    Thread clientThread = new Thread(() => {
+                        lock(clientCountLock){ this.numOfClients++; }
+                        this.handleClient(connection);
+                    });
+
+                    lock(threadCountLock){
+                        threads.Add(clientThread);
+                    }
+
+                    clientThread.Start();
+                }
+            } catch (Exception e) { Console.Out.WriteLine("[Server] Preparation: {0}", e.Message); }
+        }
+        public override string processMessage(String msg)
+        {
+            // todo [Assignment]: implement required code
+            Thread.Sleep(settings.serverProcessingTime);
+            string replyMsg = Message.confirmed;
+
+            try
+            {
+                switch (msg)
+                {
+                    case Message.terminate:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("[Server] received from the client -> {0} ", msg);
+                        Console.ResetColor();
+                        lock(clientCountLock){
+                            Console.WriteLine("[Server] END : number of clients communicated -> {0} ", this.numOfClients);
+                        }
+
+                        lock (threadCountLock){
+                            int count = threads.Count - 1;
+                            for(int i = 0; i < count; i++){
+                                threads[0].Join();
+                                threads.RemoveAt(0);
+                            }
+                        }
+
+                        Dictionary<string, int> countedVotes = new Dictionary<string, int>();
+                        lock (votesLock)
+                        {
+                            foreach(string vote in votes){
+                                Console.WriteLine("[Server] Counted vote: {0}", vote);
+                                if (countedVotes.ContainsKey(vote)) {
+                                    countedVotes[vote] += 1;
+                                } else {
+                                    countedVotes.Add(vote, 1);
+                                }
+                            }
+                        }
+
+                        KeyValuePair<string, int> maximumVote = new KeyValuePair<string, int>("", 0);
+
+                        Dictionary<string, int>.KeyCollection keys = countedVotes.Keys;
+                        foreach(string s in keys) {
+                            int value = countedVotes[s];
+                            Console.WriteLine("[Server] '{0}' has {1} votes", s, value);
+                            if (maximumVote.Value < value) {
+                                maximumVote = new KeyValuePair<string, int>(s, value);
+                            }
+                        }
+                        Console.WriteLine("[Server] maximum vote is '{0}' with {1} votes.", maximumVote.Key, maximumVote.Value);
+                        lock (threadCountLock) {
+                            threads.Clear();
+                        }
+                        lock (votesLock) {
+                            votes.Clear();
+                        }
+                        lock(clientCountLock){
+                            numOfClients = 0;
+                        }
+                        break;
+                    default:
+                        replyMsg = Message.confirmed;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("[Server] received from the client -> {0} ", msg);
+                        Console.ResetColor();
+                        string newVote = msg.Split(settings.command_msg_sep)[1];
+                        lock (votesLock) {
+                            votes.Add(newVote);
+                        }
+                        break;
+                }
+            }
+            catch (Exception e) { Console.Out.WriteLine("[Server] Process Message {0}", e.Message); }
+
+            return replyMsg;
+        }
+    }
+}
